f09c026f2b93ccb74c9b18b523e8174e299db105
ka1 <1793838025@qq.com>
2026-07-01 22:19:13 +0800

    chore(release): bump version to 3.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ahu.ahutong"
         minSdk = 26
         targetSdk = 36
-        versionCode = 317
-        versionName = "3.1.7"
+        versionCode = 318
+        versionName = "3.1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a")
